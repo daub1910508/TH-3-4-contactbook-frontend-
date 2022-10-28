ee46786046f6b469d2838e432a61fced6306318e
dau <daub1910508@student.ctu.edu.vn>
2022-10-28 22:16:03 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,23 @@
 import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
 const routes = [
-    {
-        path: "/:pathMatch(.*)*",
-        name: "notfound",
-        component: () => import("@/views/NotFound.vue"),
-        },
-        
-    {
-        path: "/",
-        name: "contactbook",
-        component: ContactBook,
-    },
+  {
+    path: "/contacts/:id",
+    name: "contact.edit",
+    component: () => import("@/views/ContactEdit.vue"),
+    props: true, // Truyền các biến trong $route.params vào làm props
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
+  },
+
+  {
+    path: "/",
+    name: "contactbook",
+    component: ContactBook,
+  },
 ];
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
